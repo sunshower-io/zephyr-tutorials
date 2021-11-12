82a6a51e5672e7019181894608156d25e3d7f000
Josiah Haswell <josiah.haswell@gmail.com>
2021-11-12 12:45:02 -0700

    Adding module activator stuff

--- lessons/1/hello-world/src/main/java/io/zephyr/lessons/helloworld/HelloWorldModuleActivator.java
+++ lessons/1/hello-world/src/main/java/io/zephyr/lessons/helloworld/HelloWorldModuleActivator.java
@@ -1,5 +1,18 @@
 package io.zephyr.lessons.helloworld;
 
-public class HelloWorldModuleActivator {
+import io.zephyr.api.ModuleActivator;
+import io.zephyr.api.ModuleContext;
 
+public class HelloWorldModuleActivator implements ModuleActivator {
+
+
+  @Override
+  public void start(ModuleContext context) {
+    System.out.println(String.format("Hello from %s", context.getModule().getCoordinate()));
+  }
+
+  @Override
+  public void stop(ModuleContext context) {
+    System.out.println(String.format("Goodbye from %s", context.getModule().getCoordinate()));
+  }
 }
