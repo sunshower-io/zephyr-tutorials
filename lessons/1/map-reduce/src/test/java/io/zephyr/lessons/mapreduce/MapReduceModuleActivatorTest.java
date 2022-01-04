e3b67944412862b994e0eb55ca6baa3f5b9f25e0
Josiah Haswell <josiah.haswell@gmail.com>
2022-01-04 13:49:27 -0700

    Updating to correct version of spring

--- lessons/1/map-reduce/src/test/java/io/zephyr/lessons/mapreduce/MapReduceModuleActivatorTest.java
+++ lessons/1/map-reduce/src/test/java/io/zephyr/lessons/mapreduce/MapReduceModuleActivatorTest.java
@@ -1,11 +1,27 @@
 package io.zephyr.lessons.mapreduce;
 
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.zephyr.kernel.concurrency.WorkerPool;
+import javax.inject.Inject;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ModuleConfiguration.class)
 class MapReduceModuleActivatorTest {
+
+
+  @Inject
+  private WorkerPool workerPool;
+
+  @Test
+  void ensureStuffWorks() {
+    assertNotNull(workerPool, "workerpool must be injected");
+
+  }
 
 
 }
