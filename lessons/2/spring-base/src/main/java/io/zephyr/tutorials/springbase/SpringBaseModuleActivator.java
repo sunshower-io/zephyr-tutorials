597f4b1ec278a40a37223853412c9de794f42664
Josiah Haswell <josiah.haswell@gmail.com>
2022-02-16 17:42:59 -0700

    Looks like stuff works

--- lessons/2/spring-base/src/main/java/io/zephyr/tutorials/springbase/SpringBaseModuleActivator.java
+++ lessons/2/spring-base/src/main/java/io/zephyr/tutorials/springbase/SpringBaseModuleActivator.java
@@ -2,10 +2,15 @@ package io.zephyr.tutorials.springbase;
 
 import io.zephyr.api.ModuleActivator;
 import io.zephyr.api.ModuleContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBaseModuleActivator implements ModuleActivator {
+
+  private ConfigurableApplicationContext context;
 
   public static void main(String[] args) {
 
@@ -14,11 +19,14 @@ public class SpringBaseModuleActivator implements ModuleActivator {
   @Override
   public void start(ModuleContext moduleContext) throws Exception {
     System.out.println("Spring base activated!");
+    SpringApplication app = new SpringApplication(SpringBaseModuleActivator.class);
+    app.setWebApplicationType(WebApplicationType.NONE);
+    this.context = app.run();
   }
 
   @Override
   public void stop(ModuleContext moduleContext) throws Exception {
+    context.stop();
     System.out.println("Spring base deactivated!");
-
   }
 }
--- lessons/2/test-module-1/src/main/java/io/zephyr/tutorials/testplugin1/Tutorial1ModuleActivator.java
+++ lessons/2/test-module-1/src/main/java/io/zephyr/tutorials/testplugin1/Tutorial1ModuleActivator.java
@@ -2,17 +2,27 @@ package io.zephyr.tutorials.testplugin1;
 
 import io.zephyr.api.ModuleActivator;
 import io.zephyr.api.ModuleContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@SpringBootApplication
 public class Tutorial1ModuleActivator implements ModuleActivator {
+
+  private ConfigurableApplicationContext context;
 
   @Override
   public void start(ModuleContext moduleContext) throws Exception {
     System.out.println("Module 1 activated!");
+    SpringApplication app = new SpringApplication(Tutorial1ModuleActivator.class);
+    app.setWebApplicationType(WebApplicationType.NONE);
+    this.context = app.run();
   }
 
   @Override
   public void stop(ModuleContext moduleContext) throws Exception {
+    context.close();
     System.out.println("Module 1 deactivated!");
-
   }
 }
